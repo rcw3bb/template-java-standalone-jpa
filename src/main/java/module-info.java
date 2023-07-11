2c3351224bbeca26851e8f6b03ea45c44e06878e
Ron Webb <ron@ronella.xyz>
2023-07-12 10:21:47 +1200

    Add usage of lombok.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module standalone.jpa {
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
+    requires static lombok;
 
     opens xyz.ronella.template.jpa to org.hibernate.orm.core;
     opens xyz.ronella.template.jpa.model to org.hibernate.orm.core;
--- src/main/java/xyz/ronella/template/jpa/model/UserInfo.java
+++ src/main/java/xyz/ronella/template/jpa/model/UserInfo.java
@@ -1,7 +1,11 @@
 package xyz.ronella.template.jpa.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Data
 @Entity
 public class UserInfo {
     @Id
@@ -9,30 +13,4 @@ public class UserInfo {
     private Long id;
     private String name;
     private String email;
-
-    // Getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
